31c03312e879132a6e14ea001fb6561cb33bbe4f
Brogolem35 <brogolem35@protonmail.com>
2024-05-27 09:13:02 +0300

    Remove dots from arg desc

--- src/args.rs
+++ src/args.rs
@@ -3,14 +3,14 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(version)]
 pub struct Cli {
-    /// Files that the words will be counted from.
+    /// Files that the words will be counted from
     pub files: Vec<String>,
 
-    /// Custom label for the `total_count` column.
+    /// Custom label for the `total_count` column
     #[arg(short, long, default_value_t = {"total_count".to_string()}, value_name = "COLUMN_LABEL")]
     pub total: String,
 
-    /// Disables the `total_count` column.
+    /// Disables the `total_count` column
     #[arg(long)]
     pub no_total: bool,
 }
