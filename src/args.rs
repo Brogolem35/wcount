97ad59843aa2c8a999f99077344dde220e3c7aa0
Brogolem35 <brogolem35@protonmail.com>
2024-05-27 09:24:35 +0300

    Reformat

--- src/args.rs
+++ src/args.rs
@@ -3,76 +3,76 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[clap(version)]
 pub struct Cli {
-    /// Files that the words will be counted from
-    pub files: Vec<String>,
+	/// Files that the words will be counted from
+	pub files: Vec<String>,
 
-    /// Custom label for the `total_count` column
-    #[arg(short, long, default_value_t = {"total_count".to_string()}, value_name = "COLUMN_LABEL")]
-    pub total: String,
+	/// Custom label for the `total_count` column
+	#[arg(short, long, default_value_t = {"total_count".to_string()}, value_name = "COLUMN_LABEL")]
+	pub total: String,
 
-    /// Disables the `total_count` column
-    #[arg(long)]
-    pub no_total: bool,
+	/// Disables the `total_count` column
+	#[arg(long)]
+	pub no_total: bool,
 
-    /// Enable `total_count` column even if only one file is read
-    /// Overrides --no-total
-    #[arg(long, verbatim_doc_comment)]
-    pub force_total: bool,
+	/// Enable `total_count` column even if only one file is read
+	/// Overrides --no-total
+	#[arg(long, verbatim_doc_comment)]
+	pub force_total: bool,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use clap::{CommandFactory, FromArgMatches};
+	use super::*;
+	use clap::{CommandFactory, FromArgMatches};
 
-    #[test]
-    fn test_default_total() {
-        let cmd = Cli::command();
-        let matches = cmd.get_matches_from(vec![
-            "wcount", // executable name
-            "file1.txt",
-            "file2.txt",
-        ]);
+	#[test]
+	fn test_default_total() {
+		let cmd = Cli::command();
+		let matches = cmd.get_matches_from(vec![
+			"wcount", // executable name
+			"file1.txt",
+			"file2.txt",
+		]);
 
-        let cli = Cli::from_arg_matches(&matches).unwrap();
+		let cli = Cli::from_arg_matches(&matches).unwrap();
 
-        assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
-        assert_eq!(cli.total, "total_count");
-        assert!(!cli.no_total);
-    }
+		assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
+		assert_eq!(cli.total, "total_count");
+		assert!(!cli.no_total);
+	}
 
-    #[test]
-    fn test_custom_total() {
-        let cmd = Cli::command();
-        let matches = cmd.get_matches_from(vec![
-            "wcount", // executable name
-            "file1.txt",
-            "file2.txt",
-            "--total",
-            "custom_label",
-        ]);
+	#[test]
+	fn test_custom_total() {
+		let cmd = Cli::command();
+		let matches = cmd.get_matches_from(vec![
+			"wcount", // executable name
+			"file1.txt",
+			"file2.txt",
+			"--total",
+			"custom_label",
+		]);
 
-        let cli = Cli::from_arg_matches(&matches).unwrap();
+		let cli = Cli::from_arg_matches(&matches).unwrap();
 
-        assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
-        assert_eq!(cli.total, "custom_label");
-        assert!(!cli.no_total);
-    }
+		assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
+		assert_eq!(cli.total, "custom_label");
+		assert!(!cli.no_total);
+	}
 
-    #[test]
-    fn test_no_total() {
-        let cmd = Cli::command();
-        let matches = cmd.get_matches_from(vec![
-            "wcount", // executable name
-            "file1.txt",
-            "file2.txt",
-            "--no-total",
-        ]);
+	#[test]
+	fn test_no_total() {
+		let cmd = Cli::command();
+		let matches = cmd.get_matches_from(vec![
+			"wcount", // executable name
+			"file1.txt",
+			"file2.txt",
+			"--no-total",
+		]);
 
-        let cli = Cli::from_arg_matches(&matches).unwrap();
+		let cli = Cli::from_arg_matches(&matches).unwrap();
 
-        assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
-        assert_eq!(cli.total, "total_count");
-        assert!(cli.no_total);
-    }
+		assert_eq!(cli.files, vec!["file1.txt", "file2.txt"]);
+		assert_eq!(cli.total, "total_count");
+		assert!(cli.no_total);
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,27 @@ use clap::Parser;
 mod args;
 
 fn main() {
-    let cargs = args::Cli::parse(); // CLI arguments
+	let cargs = args::Cli::parse(); // CLI arguments
 
-    let files = cargs.files;
+	let files = cargs.files;
 
-    let paths: Vec<PathBuf> = files
-        .iter()
-        .map(|f| PathBuf::from(f))
-        .filter(|f| {
-            if !f.exists() {
-                eprintln!("{}: No such file or directory", f.to_string_lossy());
-                false
-            } else if f.is_dir() {
-                eprintln!("{}: Is a directory", f.to_string_lossy());
-                false
-            } else {
-                f.is_file()
-            }
-        })
-        .collect();
+	let paths: Vec<PathBuf> = files
+		.iter()
+		.map(|f| PathBuf::from(f))
+		.filter(|f| {
+			if !f.exists() {
+				eprintln!("{}: No such file or directory", f.to_string_lossy());
+				false
+			} else if f.is_dir() {
+				eprintln!("{}: Is a directory", f.to_string_lossy());
+				false
+			} else {
+				f.is_file()
+			}
+		})
+		.collect();
 
-    for p in paths {
-        println!("{}", p.to_string_lossy());
-    }
+	for p in paths {
+		println!("{}", p.to_string_lossy());
+	}
 }
