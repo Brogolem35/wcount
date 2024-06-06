2896a4a3200613b1585f15fe3f461b0ad11f3214
Brogolem35 <brogolem35@protonmail.com>
2024-06-06 20:55:02 +0300

    Migrate Stream to seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod args;
+mod stream;
 use std::{
 	collections::HashMap,
 	fs::{self, read_to_string, File},
@@ -11,66 +12,8 @@ use args::Cli;
 use clap::{builder::Str, Parser};
 use once_cell::sync::Lazy;
 use regex::Regex;
+use stream::Stream;
 use ustr::{ustr, Ustr};
-
-#[derive(Debug)]
-enum Stream {
-	Stdin(io::Stdin),
-	File(File, String),
-}
-
-impl Stream {
-	fn from_str(path: &str) -> Option<Stream> {
-		if path == "-" {
-			return Some(Stream::Stdin(io::stdin()));
-		}
-
-		match fs::metadata(path) {
-			Ok(meta) => {
-				if meta.is_file() {
-					if let Ok(file) = File::open(path) {
-						Some(Stream::File(file, path.to_string()))
-					} else {
-						eprintln!("{}: Error accessing", path);
-						None
-					}
-				} else if meta.is_dir() {
-					eprintln!("{}: Is a directory", path);
-					None
-				} else {
-					eprintln!("{}: Error accessing", path);
-					None
-				}
-			}
-			Err(e) => {
-				eprintln!("{}: {}", path, e);
-				None
-			}
-		}
-	}
-
-	#[allow(dead_code)]
-	fn read_to_string(&mut self) -> Option<String> {
-		let mut buf = String::new();
-
-		match self {
-			Stream::Stdin(si) => {
-				if si.read_to_string(&mut buf).is_err() {
-					eprintln!("{}: invalid UTF-8", "-");
-					return None;
-				}
-			}
-			Stream::File(f, n) => {
-				if f.read_to_string(&mut buf).is_err() {
-					eprintln!("{}: invalid UTF-8", n);
-					return None;
-				}
-			}
-		};
-
-		Some(buf)
-	}
-}
 
 fn main() {
 	let cargs = args::Cli::parse(); // CLI arguments
@@ -177,7 +120,8 @@ mod tests {
 
 	#[test]
 	fn word_count2() {
-		let res = count_words("lorem dolor ipsum dolor. lorem? dolor dolor".to_owned()).unwrap();
+		let res = count_words("lorem dolor ipsum dolor. lorem? dolor dolor".to_owned())
+			.unwrap();
 
 		assert_eq!(res[&ustr("lorem")], 2);
 		assert_eq!(res[&ustr("ipsum")], 1);
---
+++ src/stream.rs
@@ -0,0 +1,63 @@
+use std::{
+	fs::{self, File},
+	io::{self, Read},
+};
+
+#[derive(Debug)]
+pub enum Stream {
+	Stdin(io::Stdin),
+	File(File, String),
+}
+
+impl Stream {
+	pub fn from_str(path: &str) -> Option<Stream> {
+		if path == "-" {
+			return Some(Stream::Stdin(io::stdin()));
+		}
+
+		match fs::metadata(path) {
+			Ok(meta) => {
+				if meta.is_file() {
+					if let Ok(file) = File::open(path) {
+						Some(Stream::File(file, path.to_string()))
+					} else {
+						eprintln!("{}: Error accessing", path);
+						None
+					}
+				} else if meta.is_dir() {
+					eprintln!("{}: Is a directory", path);
+					None
+				} else {
+					eprintln!("{}: Error accessing", path);
+					None
+				}
+			}
+			Err(e) => {
+				eprintln!("{}: {}", path, e);
+				None
+			}
+		}
+	}
+
+	#[allow(dead_code)]
+	pub fn read_to_string(&mut self) -> Option<String> {
+		let mut buf = String::new();
+
+		match self {
+			Stream::Stdin(si) => {
+				if si.read_to_string(&mut buf).is_err() {
+					eprintln!("{}: invalid UTF-8", "-");
+					return None;
+				}
+			}
+			Stream::File(f, n) => {
+				if f.read_to_string(&mut buf).is_err() {
+					eprintln!("{}: invalid UTF-8", n);
+					return None;
+				}
+			}
+		};
+
+		Some(buf)
+	}
+}
