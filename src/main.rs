5d7a6f317cd75ed2e245bfe1c58722c2f782d848
brogolem35 <brogolem35@protonmail.com>
2024-07-05 10:34:53 +0300

    Enforce --werror

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,37 @@ use regexes::DEFAULT_REGEX;
 use stream::Stream;
 use ustr::Ustr;
 
+enum Return {
+	Ok = 0,
+	Warning = 1,
+	Error = 2,
+}
+
 fn main() {
+	let mut result = Return::Ok;
+
 	let cargs = args::Cli::parse(); // CLI arguments
 
 	let files = &cargs.files;
 
 	if files.is_empty() {
 		eprintln!("No files entered");
-		exit(1);
+		exit(Return::Error as i32);
 	}
 
 	let streams: Vec<_> = files.iter().filter_map(|f| Stream::from_str(f)).collect();
 
 	if streams.is_empty() {
 		eprintln!("Args does not contain any valid files to process");
-		exit(1);
+		exit(Return::Error as i32);
+	}
+
+	if files.len() != streams.len() {
+		result = Return::Warning;
+
+		if cargs.werror {
+			exit(Return::Warning as i32);
+		}
 	}
 
 	let counts: Vec<_> = streams
@@ -75,4 +91,6 @@ fn main() {
 	}
 
 	wtr.flush().expect("Could not output the result");
+
+	exit(result as i32);
 }
