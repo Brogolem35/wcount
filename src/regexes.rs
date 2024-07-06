865a9bcaf8361bc501b51379128ac58ac9285ba0
Brogolem35 <brogolem35@protonmail.com>
2024-07-06 17:37:34 +0300

    Add ALPHANUMERIC_REGEX

--- src/regexes.rs
+++ src/regexes.rs
@@ -4,6 +4,9 @@ use regex::Regex;
 pub static ALL_REGEX: Lazy<Regex> =
 	Lazy::new(|| Regex::new(r"(\p{Alphabetic}|\d)(\p{Alphabetic}|\d|'|-)*").unwrap());
 
+pub static ALPHANUMERIC_REGEX: Lazy<Regex> =
+	Lazy::new(|| Regex::new(r"(\p{Alphabetic}|\d)+").unwrap());
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -56,5 +59,65 @@ mod tests {
 			.collect();
 
 		assert_eq!(rres, vec!["ömür", "ğğğ", "式", "2d3"]);
+	}
+
+	#[test]
+	fn alphanumeric1() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("lorem ipsum dolor")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ipsum", "dolor"]);
+	}
+
+	#[test]
+	fn alphanumeric2() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("lor.em ips!um 'dolor")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lor", "em", "ips", "um", "dolor"]);
+	}
+
+	#[test]
+	fn alphanumeric3() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("lorem ipsum dol_3or")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ipsum", "dol", "3or"]);
+	}
+
+	#[test]
+	fn alphanumeric4() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("123  1,23 1_2 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["123", "1", "23", "1", "2", "2d3"]);
+	}
+
+	#[test]
+	fn alphanumeric5() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("ömür ğğğ 式 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["ömür", "ğğğ", "式", "2d3"]);
+	}
+
+	#[test]
+	fn alphanumeric6() {
+		let rres: Vec<_> = ALPHANUMERIC_REGEX
+			.find_iter("lorem ip-sum dol3'or")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ip", "sum", "dol3", "or"]);
 	}
 }
