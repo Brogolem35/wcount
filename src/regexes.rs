3b139fb569d32770878508c2f5f6e27994760700
Brogolem35 <brogolem35@protonmail.com>
2024-07-06 21:37:15 +0300

    Add NOAPOSTROPHE_REGEX

--- src/regexes.rs
+++ src/regexes.rs
@@ -14,6 +14,9 @@ pub static NUMERIC_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"(\d)+").unwrap
 pub static NODASH_REGEX: Lazy<Regex> =
 	Lazy::new(|| Regex::new(r"(\p{Alphabetic}|\d)(\p{Alphabetic}|\d|')*").unwrap());
 
+pub static NOAPOSTROPHE_REGEX: Lazy<Regex> =
+	Lazy::new(|| Regex::new(r"(\p{Alphabetic}|\d)(\p{Alphabetic}|\d|-)*").unwrap());
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -306,5 +309,65 @@ mod tests {
 			.collect();
 
 		assert_eq!(rres, vec!["lorem", "ip", "sum", "dol3'or"]);
+	}
+
+	#[test]
+	fn no_apostrophe1() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("lorem ipsum dolor")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ipsum", "dolor"]);
+	}
+
+	#[test]
+	fn no_apostrophe2() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("lor.em ips!um 'dolor")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lor", "em", "ips", "um", "dolor"]);
+	}
+
+	#[test]
+	fn no_apostrophe3() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("lorem ipsum dol_3or")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ipsum", "dol", "3or"]);
+	}
+
+	#[test]
+	fn no_apostrophe4() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("123  1,23 1_2 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["123", "1", "23", "1", "2", "2d3"]);
+	}
+
+	#[test]
+	fn no_apostrophe5() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("ömür ğğğ 式 2d3")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["ömür", "ğğğ", "式", "2d3"]);
+	}
+
+	#[test]
+	fn no_apostrophe6() {
+		let rres: Vec<_> = NOAPOSTROPHE_REGEX
+			.find_iter("lorem ip-sum dol3'or")
+			.map(|m| m.as_str())
+			.collect();
+
+		assert_eq!(rres, vec!["lorem", "ip-sum", "dol3", "or"]);
 	}
 }
